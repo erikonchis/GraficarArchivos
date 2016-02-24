b275b5163c2368d81043f32a2956d1904d458eaf
Erikonchis <erikarodbor@ciencias.unam.mx>
2016-02-24 15:23:57 -0600

    Grafica y estadisitca de datos almacenados en un archivo, el cual es seleccionado por el usuario

---
+++ GraficarArchivo.R
@@ -0,0 +1,21 @@
+library (shiny)
+library(psych)
+
+ui<-fluidPage(
+  fileInput("archivo", "Elige un archivo", multiple = FALSE),
+  plotOutput("grafica"),
+  tableOutput("resumen")
+)
+
+server<-function(input, output){
+  datos<-reactive({
+    archivog<-input$archivo
+    if (is.null(archivog)) return (NULL)
+    read.csv(archivog$datapath, sep="", header=TRUE)
+  })
+  output$grafica<-renderPlot({plot(datos())})
+  output$resumen<-renderTable(describe(datos()))
+  datos
+}
+
+shinyApp(ui, server)  
